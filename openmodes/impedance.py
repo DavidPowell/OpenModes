Eigencurrents are now correctly defined, such that they yield the correct eigenimpedance when weighting the full impedance matrix to create the reduced impedance matrix. Test example to show that it works.

openmodes/impedance.py
@@ -92,10 +92,16 @@ class EfieImpedanceMatrix(object):
         Therefore this routine can return junk results, particularly if the
         mesh is dense.
         """
-        
+
         if use_gram:
-            G = self.basis_o.gram_matrix        
-            z_all, v_all = la.eig(self[:], G)
+            Gw, Gv = self.basis_o.gram_factored
+            Gwm = np.diag(1.0/Gw)
+            Zd = Gwm.dot(Gv.T.dot(self[:].dot(Gv.dot(Gwm))))
+            z_all, v_all = la.eig(Zd)
+            #v_all = Gwm.dot(Gv.T.dot(v_all))
+            #v_all = Gv.dot(Gwm.dot(v_all))
+            #G = self.basis_o.gram_matrix
+            #z_all, v_all = la.eig(self[:], G)
         else:
             z_all, v_all = la.eig(self[:])
 
@@ -104,6 +110,11 @@ class EfieImpedanceMatrix(object):
 
         v = v_all[:, which_z]
         eigencurrent = v/np.sqrt(np.sum(v**2, axis=0))
+
+        if use_gram:
+            eigencurrent = Gv.dot(Gwm.dot(eigencurrent))
+            #eigencurrent = Gwm.dot(Gv.T.dot(eigencurrent))
+
 
         return eigenimpedance, eigencurrent
 

test/test.py
@@ -171,7 +171,7 @@ def sem_eem_bcsrr():
     """
 
     srr = openmodes.load_mesh(
-                    osp.join("..", "examples", "geometry", "SRR.geo"), mesh_tol=0.7e-3)
+                    osp.join("..", "examples", "geometry", "SRR.geo"), mesh_tol=1e-3)
 
     sim = openmodes.Simulation(basis_class=LoopStarBasis)
     #sim = openmodes.Simulation(basis_class=DivRwgBasis)
@@ -180,11 +180,11 @@ def sem_eem_bcsrr():
     #srr2.rotate([0, 0, 1], 180)
 
     num_freqs = 201
-    freqs = np.linspace(0.5e9, 30e9, num_freqs)#+1e8j
+    freqs = np.linspace(0.5e9, 20e9, num_freqs)#+1e8j
     e_inc = np.array([1, 0, 0], dtype=np.complex128)
     k_hat = np.array([0, 1, 0], dtype=np.complex128)
 
-    num_modes = 6
+    num_modes = 3
 
     ext = np.empty(num_freqs, np.complex128)
     ext_modes = np.empty(num_freqs, np.complex128)
@@ -195,13 +195,14 @@ def sem_eem_bcsrr():
     #mode_s, mode_j = sim.system_singularities(s_start, num_modes)
     #models = sim.construct_model_system(mode_s, mode_j)
 
-#    mode_s, mode_j = sim.part_singularities(s_start, num_modes)
-#    print [s/(2*np.pi) for s in mode_s]
-#    models = sim.construct_models(mode_s, mode_j)[0]
+    mode_s, mode_j = sim.part_singularities(s_start, num_modes)
+    print [s/(2*np.pi) for s in mode_s]
+    models = sim.construct_models(mode_s, mode_j)[0]
     
-#    print [model.coefficients for model in models]
+    print [model.coefficients for model in models]
     
     z_eem = np.empty((num_freqs, num_modes), np.complex128)
+    z_modes = np.empty((num_freqs, num_modes), np.complex128)
     z_sem = np.empty((num_freqs, num_modes), np.complex128)
 
     for freq_count, freq in enumerate(freqs):
@@ -225,40 +226,45 @@ def sem_eem_bcsrr():
         Z_modes = Z.impedance_modes(num_modes, eem_j)
         V_modes = Z.source_modes(V, num_modes, eem_j)
         
+        z_modes[freq_count] = Z_modes[:].diagonal()        
+        
         ext_modes[freq_count] = np.vdot(V_modes, Z_modes.solve(V_modes))
 
         
         for mode in xrange(num_modes):
-#            ext_sem[freq_count, mode] = np.vdot(V, models[mode].solve(s, V))
+            ext_sem[freq_count, mode] = np.vdot(V, models[mode].solve(s, V))
             ext_eem[freq_count, mode] = np.vdot(V, eem_j[:, mode])*np.dot(V, eem_j[:, mode])/eem_z[mode]
 
-#            z_sem[freq_count, mode] = models[mode].scalar_impedance(s)
+            z_sem[freq_count, mode] = models[mode].scalar_impedance(s)
 
-    plt.figure(figsize=(10, 5))
-    plt.subplot(121)
-    plt.plot(freqs*1e-9, ext.real)
-    plt.plot(freqs*1e-9, ext_modes.real)
-    #plt.plot(freqs*1e-9, np.sum(ext_sem.real, axis=1))
-    plt.plot(freqs*1e-9, np.sum(ext_eem.real, axis=1))
-    plt.subplot(122)
-    plt.plot(freqs*1e-9, ext.imag)
-    plt.plot(freqs*1e-9, ext_modes.imag)
-    #plt.plot(freqs*1e-9, np.sum(ext_sem.imag, axis=1))
-    plt.plot(freqs*1e-9, np.sum(ext_eem.imag, axis=1))
-    plt.show()
+#    plt.figure(figsize=(10, 5))
+#    plt.subplot(121)
+#    plt.plot(freqs*1e-9, ext.real)
+#    plt.plot(freqs*1e-9, ext_modes.real)
+#    #plt.plot(freqs*1e-9, np.sum(ext_sem.real, axis=1))
+#    plt.plot(freqs*1e-9, np.sum(ext_eem.real, axis=1))
+#    plt.subplot(122)
+#    plt.plot(freqs*1e-9, ext.imag)
+#    plt.plot(freqs*1e-9, ext_modes.imag)
+#    #plt.plot(freqs*1e-9, np.sum(ext_sem.imag, axis=1))
+#    plt.plot(freqs*1e-9, np.sum(ext_eem.imag, axis=1))
+#    plt.show()
     
 #    z_eem = 1.0/z_eem
 #    z_sem = 1.0/z_sem    
     
-#    plt.figure(figsize=(7, 3))
-#    plt.subplot(121)
-#    plt.plot(freqs*1e-9, z_eem.real, 'x')
-##    plt.plot(freqs*1e-9, z_sem.real, '--')
-#    plt.subplot(122)
-#    plt.semilogy(freqs*1e-9, abs(z_eem.imag), 'x')
-#    #plt.plot(freqs*1e-9, z_eem.imag, 'x')
-##    plt.plot(freqs*1e-9, z_sem.imag, '--')
-#    plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.subplot(121)
+    plt.plot(freqs*1e-9, z_eem.real, 'x')
+    plt.plot(freqs*1e-9, z_modes.real, '--')
+    #plt.plot(freqs*1e-9, z_sem.real, '--')
+    plt.subplot(122)
+    plt.semilogy(freqs*1e-9, abs(z_eem.imag), 'x')
+    plt.semilogy(freqs*1e-9, abs(z_modes.imag), '--')
+   # plt.semilogy(freqs*1e-9, abs(z_sem.imag), '--')
+    #plt.plot(freqs*1e-9, z_eem.imag, 'x')
+    #plt.plot(freqs*1e-9, z_sem.imag, '--')
+    plt.show()
 
 
 def srr_coupling():