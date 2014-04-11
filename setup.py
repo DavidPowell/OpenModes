c5adea9820f2c74603b9c353bca1932712be6860
David Powell <david.a.powell@anu.edu.au>
2014-04-11 22:02:22 +1000

    PEP8 and correcting location of readme

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ from numpy.distutils.core import Extension, setup
 # packaging a whole bunch of dlls. However, static linking is not supported
 # for the openmp libraries.
 ccompiler_dependent_options = {
-    'mingw32' : {
+    'mingw32': {
     #    'extra_link_args' : ['-static']
     }
 }
@@ -59,15 +59,15 @@ fcompiler_dependent_options = {
         'extra_f90_compile_args': ["-g", "-fimplicit-none", "-fopenmp", "-O1"],
         'libraries': ["gomp"]
      },
-        
+
     'intel': {
-        # -O3 also causes NaNs under intel fortran
-        'extra_f90_compile_args' : ['-openmp', '-O2'],
-        #'extra_link_args' : ['-openmp']
-        #'extra_f77_compile_args' : ['-openmp', '-O3'],
-        #'extra_compile_args' : ['-openmp', '-O3', '-static'],
-        #'extra_link_args' : ['-nodefaultlib:msvcrt']
-    }     
+              # -O3 also causes NaNs under intel fortran
+              'extra_f90_compile_args': ['-openmp', '-O2'],
+              #'extra_link_args' : ['-openmp']
+              #'extra_f77_compile_args' : ['-openmp', '-O3'],
+              #'extra_compile_args' : ['-openmp', '-O3', '-static'],
+              #'extra_link_args' : ['-nodefaultlib:msvcrt']
+    }
 }
 
 # Intel fortran compiler goes by several names depending on the version
@@ -75,47 +75,48 @@ fcompiler_dependent_options = {
 fcompiler_dependent_options['intelem'] = fcompiler_dependent_options['intel']
 fcompiler_dependent_options['intelvem'] = fcompiler_dependent_options['intel']
 
-core = Extension(name = 'openmodes.core',
-                 sources = [osp.join('src', 'core.pyf'),
-                            osp.join('src', 'common.f90'),
-                            osp.join('src', 'rwg.f90')],
-                )
+core = Extension(name='openmodes.core',
+                 sources=[osp.join('src', 'core.pyf'),
+                          osp.join('src', 'common.f90'),
+                          osp.join('src', 'rwg.f90')],
+                 )
 
-dunavant = Extension(name = 'openmodes.dunavant',
+dunavant = Extension(name='openmodes.dunavant',
                      sources=[osp.join('src', 'dunavant.pyf'),
                               osp.join('src', 'dunavant.f90')])
 
 from numpy.distutils.command.build_ext import build_ext
 
-class compiler_dependent_build_ext( build_ext ):
+
+class compiler_dependent_build_ext(build_ext):
     """A build extension which allows compiler-dependent options for
     compilation, linking etc. Options can depend on either the C or FORTRAN
     compiler which is actually used (as distinct from the default compilers,
     which are much easier to detect)
-    
+
     Based on http://stackoverflow.com/a/5192738/482420
     """
-    
+
     def build_extensions(self):
         ccompiler = self.compiler.compiler_type
-        fcompiler = self._f77_compiler.compiler_type            
+        fcompiler = self._f77_compiler.compiler_type
 
         # add the compiler dependent options to each extension
         for extension in self.extensions:
-            try:        
+            try:
                 modification = ccompiler_dependent_options[ccompiler]
                 for key, val in modification.iteritems():
                     getattr(extension, key).extend(val)
             except KeyError:
                 pass
-            
-            try:        
+
+            try:
                 modification = fcompiler_dependent_options[fcompiler]
                 for key, val in modification.iteritems():
                     getattr(extension, key).extend(val)
             except KeyError:
-                pass        
-        
+                pass
+
         build_ext.build_extensions(self)
 
 # Find library files which must be included, which should be placed in the
@@ -126,24 +127,24 @@ redist_data = []
 if osp.exists(redist_path):
     redist_data.append(redist_path)
 
-with open('README.md') as description_file:
+with open('README.rst') as description_file:
     long_description = description_file.read()
 
-setup(name = 'OpenModes',
-    description = "An eigenmode solver for open electromagnetic resonantors",
-    author = "David Powell",
-    author_email = 'david.a.powell -at- anu.edu.au',
-    license ='GPLv3+',
-    url = 'https://github.com/DavidPowell/OpenModes',
-    packages = ['openmodes'],
-    package_data={'openmodes': [osp.join("geometry", "*.geo")]},
-    ext_modules = [dunavant, core],
-    version = get_git_version(),
-    install_requires = ['numpy >= 1.6.2', 'scipy', 'matplotlib'],
-    long_description=long_description,
-    platforms = "Windows, Linux",
-    use_2to3 = True,
-    classifiers=[
+setup(name='OpenModes',
+      description="An eigenmode solver for open electromagnetic resonantors",
+      author="David Powell",
+      author_email='david.a.powell -at- anu.edu.au',
+      license='GPLv3+',
+      url='https://github.com/DavidPowell/OpenModes',
+      packages=['openmodes'],
+      package_data={'openmodes': [osp.join("geometry", "*.geo")]},
+      ext_modules=[dunavant, core],
+      version=get_git_version(),
+      install_requires=['numpy >= 1.6.2', 'scipy', 'matplotlib'],
+      long_description=long_description,
+      platforms="Windows, Linux",
+      use_2to3=True,
+      classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Environment :: Web Environment',
@@ -156,9 +157,8 @@ setup(name = 'OpenModes',
           'Programming Language :: Fortran',
           'Topic :: Scientific/Engineering'
           ],
-    cmdclass = {'build_ext': compiler_dependent_build_ext},
+      cmdclass={'build_ext': compiler_dependent_build_ext},
 
-    # Include any required library files
-    data_files = [('openmodes', redist_data+["RELEASE-VERSION"])]
-    )
-
+      # Include any required library files
+      data_files=[('openmodes', redist_data+["RELEASE-VERSION"])]
+      )
