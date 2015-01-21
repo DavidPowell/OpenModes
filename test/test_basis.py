2dcf61299bbefa86bce1b9a41d85f79c47452b39
David Powell <david.a.powell@anu.edu.au>
2015-01-22 09:57:49 +1100

    Test cases for interpolation of basis functions

--- test/test_basis.py
+++ test/test_basis.py
@@ -22,121 +22,104 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os.path as osp
 
-from openmodes.basis import DivRwgBasis, LoopStarBasis#, DivRwgGramBasis
-from openmodes.integration import get_dunavant_rule
-from openmodes import load_mesh
+import openmodes
+from openmodes.basis import DivRwgBasis, LoopStarBasis
+from openmodes.integration import DunavantRule
+from openmodes import Simulation
 from openmodes.visualise import write_vtk
 
-def test_interpolate_rwg():
+from numpy.testing import assert_allclose
 
+tests_location = osp.split(__file__)[0]
+
+
+def test_interpolate_rwg(plot=False):
+    "Interpolate an RWG basis function over a triangle"
+
+    sim = Simulation()
     mesh_tol = 0.5e-3
-    srr = load_mesh(osp.join(openmodes.geometry_dir, "SRR.geo"), mesh_tol)
-    
+    srr = sim.load_mesh(osp.join(openmodes.geometry_dir, "SRR.geo"), mesh_tol)
+
     basis = DivRwgBasis(srr)
-    
+
     rwg_function = np.zeros(len(basis), np.float64)
     rwg_function[20] = 1
-    
-    xi_eta, weights = get_dunavant_rule(10)
-    
-    
-    r, res = basis.interpolate_function(rwg_function , xi_eta)
-    
-    plt.figure(figsize=(6, 6))
-    plt.quiver(r[:, 0], r[:, 1], res[:, 0], res[:, 1], scale=5e4)
-    #plt.plot(r[:, 0], r[:, 1], 'x')
-    plt.show()
 
-def test_interpolate_loop_star():
-        
+    rule = DunavantRule(10)
+
+    r, basis_func = basis.interpolate_function(rwg_function, rule)
+
+    # save reference data
+    # np.savetxt(osp.join(tests_location, 'reference', 'test_interpolate_rwg',
+    #                     'rwg_r.txt'), r, fmt="%.8e")
+    # np.savetxt(osp.join(tests_location, 'reference', 'test_interpolate_rwg',
+    #                     'rwg_basis_func.txt'), basis_func, fmt="%.8e")
+
+    r_ref = np.loadtxt(osp.join(tests_location, 'reference',
+                                'test_interpolate_rwg', 'rwg_r.txt'))
+    basis_func_ref = np.loadtxt(osp.join(tests_location, 'reference',
+                                         'test_interpolate_rwg',
+                                         'rwg_basis_func.txt'))
+    assert_allclose(r, r_ref)
+    assert_allclose(basis_func, basis_func_ref)
+
+    if plot:
+        plt.figure(figsize=(6, 6))
+        plt.quiver(r[:, 0], r[:, 1], basis_func[:, 0], basis_func[:, 1],
+                   scale=5e4)
+        plt.show()
+
+
+def test_interpolate_loop_star(plot=False):
+    "Interpolate loop and star basis functions"
+
+    sim = Simulation()
     mesh_tol = 4e-3
-    mesh = load_mesh(osp.join(openmodes.geometry_dir, "square_plate.geo"), mesh_tol)
-    
-    #basis = DivRwgBasis(mesh)
+    mesh = sim.load_mesh(osp.join(openmodes.geometry_dir, "rectangle.geo"),
+                         parameters={'width': 10e-3, 'height': 10e-3},
+                         mesh_tol=mesh_tol)
+
     basis = LoopStarBasis(mesh)
-    
-    which_basis = 22 #basis.num_loops+2
-    
+
     ls_function = np.zeros(len(basis), np.float64)
-    ls_function[which_basis] = 1
-    
-    ls_function[4] = 1
-    
-    xi_eta, weights = get_dunavant_rule(10)
-    
-    
-    r, res = basis.interpolate_function(ls_function, xi_eta)
-    
-    the_basis = basis[which_basis]
-    
+    # chose one loop and one star
+    star_basis = 22
+    loop_basis = 4
+
+    ls_function[star_basis] = 1
+    ls_function[loop_basis] = 1
+
+    rule = DunavantRule(10)
+    r, basis_func = basis.interpolate_function(ls_function, rule)
+
+    the_basis = basis[star_basis]
+
     plus_nodes = mesh.nodes[basis.mesh.polygons[the_basis.tri_p, the_basis.node_p]]
     minus_nodes = mesh.nodes[basis.mesh.polygons[the_basis.tri_m, the_basis.node_m]]
-    
-    plt.figure(figsize=(6, 6))
-    #plt.quiver(r[:, 0], r[:, 1], res[:, 0], res[:, 1], scale=0.05, pivot='middle')
-    plt.quiver(r[:, 0], r[:, 1], res[:, 0], res[:, 1], pivot='middle')
-    plt.plot(plus_nodes[:, 0], plus_nodes[:, 1], 'x')
-    plt.plot(minus_nodes[:, 0], minus_nodes[:, 1], '+')
-    #plt.plot(r[:, 0], r[:, 1], 'x')
-    plt.show()
 
-def loop_star_to_vtk():
-        
-    mesh_tol = 1e-3
-    mesh = load_mesh(osp.join(openmodes.geometry_dir, "SRR.geo"), mesh_tol)
-    
-    #basis = DivRwgBasis(mesh)
-    basis = LoopStarBasis(mesh)
-    print len(basis)    
-    
-    for basis_count in xrange(len(basis)):
-    
-        #which_basis = 22 #basis.num_loops+2
-        
-        I = np.zeros(len(basis), np.float64)
-        I[basis_count] = 1
-        
-        #ls_function[4] = 1
-        
-        xi_eta, weights = get_dunavant_rule(10)
-        
-        
-        #r, res = basis.interpolate_function(ls_function, xi_eta)
+    # save reference data
+    # np.savetxt(osp.join(tests_location, 'reference', 'test_interpolate_rwg',
+    #                     'loop_star_r.txt'), r, fmt="%.8e")
+    # np.savetxt(osp.join(tests_location, 'reference', 'test_interpolate_rwg',
+    #                     'loop_star_basis_func.txt'), basis_func, fmt="%.8e")
 
-        #xi_eta = xi_eta, 
+    r_ref = np.loadtxt(osp.join(tests_location, 'reference',
+                                'test_interpolate_rwg', 'loop_star_r.txt'))
+    basis_func_ref = np.loadtxt(osp.join(tests_location, 'reference',
+                                         'test_interpolate_rwg',
+                                         'loop_star_basis_func.txt'))
+    assert_allclose(r, r_ref)
+    assert_allclose(basis_func, basis_func_ref)
 
-        face_centre, face_current, face_charge = basis.interpolate_function(I, return_scalar=True)
-            
-        write_vtk(mesh, mesh.nodes, osp.join("output", "srr-basis-%02d.vtk" % basis_count), 
-                  vector_function = face_current, scalar_function=face_charge
-                  )        
-        
-#        the_basis = basis[which_basis]
-#        
-#        plus_nodes = mesh.nodes[basis.mesh.polygons[the_basis.tri_p, the_basis.node_p]]
-#        minus_nodes = mesh.nodes[basis.mesh.polygons[the_basis.tri_m, the_basis.node_m]]
-#        
-#        plt.figure(figsize=(6, 6))
-#        #plt.quiver(r[:, 0], r[:, 1], res[:, 0], res[:, 1], scale=0.05, pivot='middle')
-#        plt.quiver(r[:, 0], r[:, 1], res[:, 0], res[:, 1], pivot='middle')
-#        plt.plot(plus_nodes[:, 0], plus_nodes[:, 1], 'x')
-#        plt.plot(minus_nodes[:, 0], minus_nodes[:, 1], '+')
-#        #plt.plot(r[:, 0], r[:, 1], 'x')
-#        plt.show()
-#
+    if plot:
+        plt.figure(figsize=(6, 6))
+        plt.quiver(r[:, 0], r[:, 1], basis_func[:, 0], basis_func[:, 1],
+                   pivot='middle')
+        plt.plot(plus_nodes[:, 0], plus_nodes[:, 1], 'x')
+        plt.plot(minus_nodes[:, 0], minus_nodes[:, 1], '+')
+        plt.show()
 
 
-mesh_tol = 0.5e-3
-srr = load_mesh(osp.join(openmodes.geometry_dir, "SRR.geo"), mesh_tol)
-
-basis = DivRwgGramBasis(srr)
-#basis = DivRwgBasis(srr)
-
-t1, t2 = basis.transformation_matrices
-
-#print basis.transformation_matrices
-
-
-#test_interpolate_rwg()
-#test_interpolate_loop_star()
-#loop_star_to_vtk()
+if __name__ == "__main__":
+    test_interpolate_rwg(plot=True)
+    test_interpolate_loop_star(plot=True)
