d3284761556526e68e8f31c8eba41207520eb443
David Powell <david.a.powell@anu.edu.au>
2015-01-22 10:33:13 +1100

    Account for scrubbed notebooks

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -55,7 +55,7 @@ def geometry_extinction_modes(name, freqs, num_modes, mesh_tol,
         for mode in xrange(num_modes):
             current = sim.empty_vector()
             current[:] = mode_j[:, mode]
-            sim.plot_solution(current, 'mayavi', compress_scalars=1)
+            sim.plot_3d(solution=current, output_format='mayavi', compress_scalars=1)
 
     if not plot_admittance:
         return
@@ -146,8 +146,8 @@ def geometry_extinction_modes(name, freqs, num_modes, mesh_tol,
     plt.show()
 
 
-#geometry_extinction_modes('horseshoe_rect', np.linspace(1e8, 20e9, 101),
-#                          3, 1.5e-3)
+geometry_extinction_modes('horseshoe_rect', np.linspace(1e8, 20e9, 101),
+                          3, 1.5e-3, plot_currents=True)
 #geometry_extinction_modes('sphere', np.linspace(0.2e7, 8e7, 101), 8, 0.2)
 #geometry_extinction_modes('canonical_spiral', np.linspace(1e8, 15e9, 101),
 #                          3, 1e-3, parameters={'arm_length': 12e-3,
@@ -163,8 +163,8 @@ def geometry_extinction_modes(name, freqs, num_modes, mesh_tol,
 #geometry_extinction_modes('cross', np.linspace(1e8, 20e9, 101), 2, 1e-3,
 #                          plot_currents=False, plot_admittance=True)
 
-geometry_extinction_modes('closed_ring', np.linspace(1e8, 15e9, 101),
-                          2, 1e-3, model_class=ScalarModelLeastSq,
-                          plot_currents=True,
-                          parameters={'inner_radius': 3e-3,
-                                      'outer_radius': 6e-3})
+#geometry_extinction_modes('closed_ring', np.linspace(1e8, 15e9, 101),
+#                          2, 1e-3, model_class=ScalarModelLeastSq,
+#                          plot_currents=True, plot_admittance=False,
+#                          parameters={'inner_radius': 3e-3,
+#                                      'outer_radius': 6e-3})
