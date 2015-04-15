2e227e4a28e9d1b32502ff72281883f716a20be9
David Powell <david.a.powell@anu.edu.au>
2015-04-15 15:56:27 +1000

    Calculate impedance of a material

--- openmodes/material.py
+++ openmodes/material.py
@@ -21,7 +21,10 @@ Routines to describe materials
 """
 from __future__ import division
 
+import numpy as np
+
 from openmodes.helpers import Identified, wrap_if_constant
+from openmodes.constants import eta_0
 
 
 class IsotropicMaterial(Identified):
@@ -31,6 +34,14 @@ class IsotropicMaterial(Identified):
         self.name = name
         self.epsilon_r = wrap_if_constant(epsilon_r)
         self.mu_r = wrap_if_constant(mu_r)
+
+    def eta(self, s):
+        "Absolute impedance of the material"
+        return eta_0*self.eta_r(s)
+
+    def eta_r(self, s):
+        "Impedance of the material relative to free space"
+        return np.sqrt(self.mu_r(s)/self.epsilon_r(s))
 
 # a constant for free space
 FreeSpace = IsotropicMaterial("Free space", 1.0, 1.0)
