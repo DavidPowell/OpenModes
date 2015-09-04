Frequency derivatives should now be correct for EFIE in an arbitrary medium

openmodes/operator/rwg.py
@@ -102,7 +102,8 @@ def impedance_G(s, integration_rule, basis_o, nodes_o, basis_s, nodes_s,
     transform_L_o, transform_S_o = basis_o.transformation_matrices
     num_faces_o = len(basis_o.mesh.polygons)
 
-    gamma_0 = s/c*np.sqrt(epsilon*mu)
+    c_mat = c/np.sqrt(epsilon*mu)
+    gamma_0 = s/c_mat
 
     if (self_impedance):
         # calculate self impedance
@@ -157,7 +158,7 @@ def impedance_G(s, integration_rule, basis_o, nodes_o, basis_s, nodes_s,
                                                                 order='C').T).T)
     dS_ds = transform_S_o.dot(transform_S_s.dot(phi_dgamma_faces.T).T)
 
-    dL_ds /= c*4*pi
-    dS_ds /= c*pi
+    dL_ds /= c_mat*4*pi
+    dS_ds /= c_mat*pi
 
     return L, S, dL_ds, dS_ds