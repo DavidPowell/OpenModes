7d3002f358c8f11f1020a888f75c15c9161c1d36
David Powell <david.a.powell@anu.edu.au>
2016-03-21 12:40:42 +1100

    New testing functions to reduce boilerplate code

--- test/helpers.py
+++ test/helpers.py
@@ -19,6 +19,10 @@
 """Helper routines for tests"""
 
 import numpy as np
+import os.path as osp
+import os
+import pickle
+from numpy.testing import assert_allclose
 
 
 def write_1d_complex(filename, data):
@@ -59,3 +63,65 @@ def read_2d_real(filename):
             data.append([float(d) for d in row])
 
     return np.array(data, dtype=np.float64)
+
+
+def get_test_dir(tests_filename):
+    location, fname = osp.split(tests_filename)
+    fname = osp.splitext(fname)[0]
+    test_dir = osp.join(location, 'reference', fname)
+    if not osp.exists(test_dir):
+        os.makedirs(test_dir)
+    return test_dir
+
+
+def get_input_dir(tests_filename, name=None):
+    location, fname = osp.split(tests_filename)
+    fname = osp.splitext(fname)[0]
+    input_dir = osp.join(location, 'input', fname)
+    if name is not None:
+        input_dir = osp.join(input_dir, name)
+    if not osp.exists(input_dir):
+        os.makedirs(input_dir)
+    return input_dir
+
+
+def compare_ref(val, reference, rtol, name):
+    "Compare a value against a stored reference"
+    if isinstance(val, np.ndarray):
+        # Compare numpy arrays
+        assert_allclose(val, reference, rtol=rtol,
+                        err_msg='Result "{}" differs from reference'.format(name))
+    elif isinstance(val, dict):
+        # Compare dictionaries, ensuring keys and all values are the same
+        assert(set(val.keys()) == set(reference.keys()))
+        for key in val.keys():
+            compare_ref(val[key], reference[key], rtol, "{}[{}]".format(name, key))
+    else:
+        raise ValueError("Unable to compare {} of type {} to reference".format(name, type(val)))
+
+
+def run_test(func, tests_filename):
+    "Run a test function and check the output against reference"
+    results = func()
+    test_dir = get_test_dir(tests_filename)
+
+    with open(osp.join(test_dir, results['name'])+".pickle", "rb") as infile:
+        reference = pickle.load(infile)
+
+    assert(reference.keys() == results['results'].keys())
+    for name, val in results['results'].items():
+        try:
+            rtol = results['rtol'][name]
+        except:
+            rtol = 1e-7
+
+        compare_ref(val, reference[name], rtol, name)
+
+
+def create_reference(func, tests_filename):
+    "Run a test function and generate reference output"
+    results = func()
+    test_dir = get_test_dir(tests_filename)
+
+    with open(osp.join(test_dir, results['name'])+".pickle", "wb") as outfile:
+        pickle.dump(results['results'], outfile, protocol=2, fix_imports=True)
