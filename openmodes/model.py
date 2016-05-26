98e06bbbc76fbd3418e0c5d861d354d365a124e3
David Powell <david.a.powell@anu.edu.au>
2016-05-26 16:39:24 +1000

    Specific model for systems based on EFIE, allowing mutual terms to be calculated.

--- openmodes/model.py
+++ openmodes/model.py
@@ -19,7 +19,7 @@
 "Fit scalar models to numerically calculated impedance data"
 
 import numpy as np
-from openmodes.impedance import ImpedanceMatrixLA
+from openmodes.impedance import ImpedanceMatrixLA, EfieImpedanceMatrixLA
 
 
 class ModelMutualWeight(object):
@@ -37,6 +37,7 @@ class ModelMutualWeight(object):
         self.parts = list(modes.modes_of_parts.keys())
         self.parent_part = modes.parent_part
         self.macro_container = modes.macro_container
+        self.symmetric = modes.operator.reciprocal
 
     def impedance_self(self, s, part_o):
         "Self impedance of one part"
@@ -48,7 +49,7 @@ class ModelMutualWeight(object):
         vl = self.modes.vl[:, part_o, :, part_o]
         vr = self.modes.vr[:, part_s, :, part_s]
         Z = self.modes.operator.impedance(s, part_o, part_s)
-        return Z.weight(vr, vl).val().simple_view()
+        return Z.weight(vr, vl)
 
     def impedance(self, s):
         """Impedance matrix
@@ -68,5 +69,44 @@ class ModelMutualWeight(object):
                     Z.matrices['Z'][part_o, part_s] = self.impedance_self(s, part_o)
                 else:
                     Z.matrices['Z'][part_o, part_s] = self.impedance_mutual(s, part_o, part_s)
+
+        return Z
+
+
+class EfieModelMutualWeight(ModelMutualWeight):
+    """A model where mutual terms come from directly weighting the mutual
+    terms of the full impedance matrix"""
+
+    def __init__(self, modes):
+        ModelMutualWeight.__init__(self, modes)
+
+    def impedance(self, s):
+        """Impedance matrix
+
+        Parameters
+        ----------
+        s : complex
+            Frequency at which to calculate impedance
+        """
+        Z = EfieImpedanceMatrixLA(self.parent_part, self.parent_part,
+                                  self.macro_container, ('modes',), ('modes',))
+
+        Z.md['s'] = s
+        Z.md['symmetric'] = self.symmetric
+        Z.md['operator'] = self.modes.operator
+
+        for count_o, part_o in enumerate(self.parts):
+            for count_s, part_s in enumerate(self.parts):
+                if count_o == count_s:
+                    s_o = self.modes.s[0, part_o]
+                    Z.matrices['S'][part_o, part_s] = np.diag(s_o*(s-s_o))
+                    Z.matrices['L'][part_o, part_s] = 0.0
+                elif self.symmetric and (count_o > count_s):
+                    # account for symmetry of operator
+                    Z[part_o, part_s] = Z[part_s, part_o].T
+                else:
+                    mutual_terms = self.impedance_mutual(s, part_o, part_s)
+                    for name, mat in mutual_terms.matrices.items():
+                        Z.matrices[name][part_o, part_s] = mat
 
         return Z
