127b6d041ca216edb83b02115536a397d4ccdec4
David Powell <david.a.powell@anu.edu.au>
2016-05-26 17:26:28 +1000

    Simplified EFIE model by refactoring more general model

--- openmodes/model.py
+++ openmodes/model.py
@@ -38,18 +38,19 @@ class ModelMutualWeight(object):
         self.parent_part = modes.parent_part
         self.macro_container = modes.macro_container
         self.symmetric = modes.operator.reciprocal
+        self.impedance_class = ImpedanceMatrixLA
 
-    def impedance_self(self, s, part_o):
+    def impedance_self(self, s, part_o, Z_full):
         "Self impedance of one part"
         s_o = self.modes.s[0, part_o]
-        return np.diag(s_o*(s-s_o)/s)
+        Z_full.matrices['Z'][part_o, part_o] = np.diag(s_o*(s-s_o)/s)
+        # TODO: impedance derivative
 
-    def impedance_mutual(self, s, part_o, part_s):
+    def impedance_mutual(self, s, part_o, part_s, Z_full):
         "Impedance between two parts, by weighting matrix"
         vl = self.modes.vl[:, part_o, :, part_o]
         vr = self.modes.vr[:, part_s, :, part_s]
-        Z = self.modes.operator.impedance(s, part_o, part_s)
-        return Z.weight(vr, vl)
+        Z_full[part_o, part_s] = self.modes.operator.impedance(s, part_o, part_s).weight(vr, vl)
 
     def impedance(self, s):
         """Impedance matrix
@@ -59,17 +60,22 @@ class ModelMutualWeight(object):
         s : complex
             Frequency at which to calculate impedance
         """
-        Z = ImpedanceMatrixLA(self.parent_part, self.parent_part,
-                              self.macro_container, ('modes',), ('modes',))
+        Z = self.impedance_class(self.parent_part, self.parent_part,
+                                 self.macro_container, ('modes',), ('modes',))
 
-        # TODO: account for symmetry of operator
-        for part_o in self.parts:
-            for part_s in self.parts:
-                if part_o is part_s:
-                    Z.matrices['Z'][part_o, part_s] = self.impedance_self(s, part_o)
+        Z.md['s'] = s
+        Z.md['symmetric'] = self.symmetric
+        Z.md['operator'] = self.modes.operator
+
+        for count_o, part_o in enumerate(self.parts):
+            for count_s, part_s in enumerate(self.parts):
+                if count_o == count_s:
+                    self.impedance_self(s, part_o, Z)
+                elif self.symmetric and (count_o > count_s):
+                    # account for symmetry of operator
+                    Z[part_o, part_s] = Z[part_s, part_o].T
                 else:
-                    Z.matrices['Z'][part_o, part_s] = self.impedance_mutual(s, part_o, part_s)
-
+                    self.impedance_mutual(s, part_o, part_s, Z)
         return Z
 
 
@@ -79,34 +85,11 @@ class EfieModelMutualWeight(ModelMutualWeight):
 
     def __init__(self, modes):
         ModelMutualWeight.__init__(self, modes)
+        self.impedance_class = EfieImpedanceMatrixLA
 
-    def impedance(self, s):
-        """Impedance matrix
-
-        Parameters
-        ----------
-        s : complex
-            Frequency at which to calculate impedance
-        """
-        Z = EfieImpedanceMatrixLA(self.parent_part, self.parent_part,
-                                  self.macro_container, ('modes',), ('modes',))
-
-        Z.md['s'] = s
-        Z.md['symmetric'] = self.symmetric
-        Z.md['operator'] = self.modes.operator
-
-        for count_o, part_o in enumerate(self.parts):
-            for count_s, part_s in enumerate(self.parts):
-                if count_o == count_s:
-                    s_o = self.modes.s[0, part_o]
-                    Z.matrices['S'][part_o, part_s] = np.diag(s_o*(s-s_o))
-                    Z.matrices['L'][part_o, part_s] = 0.0
-                elif self.symmetric and (count_o > count_s):
-                    # account for symmetry of operator
-                    Z[part_o, part_s] = Z[part_s, part_o].T
-                else:
-                    mutual_terms = self.impedance_mutual(s, part_o, part_s)
-                    for name, mat in mutual_terms.matrices.items():
-                        Z.matrices[name][part_o, part_s] = mat
-
-        return Z
+    def impedance_self(self, s, part_o, Z_full):
+        "Self impedance of one part"
+        s_o = self.modes.s[0, part_o]
+        Z_full.matrices['S'][part_o, part_o] = np.diag(s_o*(s-s_o))
+        Z_full.matrices['L'][part_o, part_o] = 0.0
+        # TODO: derivatives
