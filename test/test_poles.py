5b0ba3a5ca715cb0e17a0dac13b81a1f5403edbc
David Powell <david.a.powell@anu.edu.au>
2017-07-07 16:01:35 +1000

    Updated SRR pair tests to use a pre-generated mesh. Fixes #3.

--- test/test_poles.py
+++ test/test_poles.py
@@ -10,100 +10,113 @@ from __future__ import print_function
 import os.path as osp
 
 import numpy as np
-from numpy.testing import assert_allclose
 
 import matplotlib.pyplot as plt
 
 import openmodes
-from openmodes.basis import DivRwgBasis, LoopStarBasis
+from openmodes.basis import LoopStarBasis
 from openmodes.operator import EfieOperator
-from openmodes.integration import RectangularContour
+from openmodes.integration import ExternalModeContour
+from openmodes.mesh import gmsh
+
+import helpers
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
 
+tests_filename = __file__
+input_dir = helpers.get_input_dir(tests_filename)
+meshfile = osp.join(input_dir, 'srr.msh')
 
-def test_srr_pair_combined_poles(plot_figures=False):
+parameters = {'inner_radius': 2.5e-3,
+              'outer_radius': 4e-3}
+
+
+def generate_mesh():
+    "Generate a fixed mesh file to ensure consistent results of tests"
+    meshed_name = gmsh.mesh_geometry(osp.join(openmodes.geometry_dir, 'srr.geo'),
+                                     input_dir, parameters=parameters)
+    assert(meshed_name == meshfile)
+
+
+
+def srr_pair_combined_poles(plot=False):
     "Cauchy integral for poles of an SRR pair considered as a single part"
 
     sim = openmodes.Simulation(basis_class=LoopStarBasis,
                                operator_class=EfieOperator)
 
-    parameters = {'inner_radius': 2.5e-3,
-                  'outer_radius': 4e-3}
-    mesh = sim.load_mesh(osp.join(openmodes.geometry_dir, 'SRR.geo'),
-                         parameters=parameters, mesh_tol=0.5e-3)
+    mesh = sim.load_mesh(meshfile)
     srr1 = sim.place_part(mesh)
     srr2 = sim.place_part(mesh, location=[0, 0, 1e-3])
 
-    s_min = -0.5e11-1e9j
-    s_max = -2e6+1.2e11j
-    integration_line_i = np.array((s_min.imag, s_max.imag, s_max.imag,
-                                   s_min.imag, s_min.imag))
-    integration_line_r = np.array((s_min.real, s_min.real, s_max.real,
-                                   s_max.real, s_min.real))
-
     # calculate modes of the SRR pair
-    result = sim.estimate_poles(RectangularContour(s_min, s_max))
+    contour = ExternalModeContour(-0.5e11+1.2e11j, overlap_axes=0.2e6)
+    result = sim.estimate_poles(contour)
     refined = sim.refine_poles(result)
 
     s_estimate = result.s
     s_refined = refined.s
 
-    s_reference = [-1.18100087e+09 + 4.30266982e+10j,
-                   -1.34656915e+07 + 4.74170628e+10j,
-                   -3.05500910e+10 + 8.60872257e+10j,
-                   -7.57094449e+08 + 9.52970974e+10j]
-
-    assert_allclose(s_refined[0], s_reference, rtol=1e-5)
-
-    if plot_figures:
+    if plot:
         plt.figure(figsize=(6, 4))
         plt.plot(s_estimate.imag, s_estimate.real, 'x')
         plt.plot(s_refined.imag, s_refined.real, '+')
-        plt.plot(integration_line_i, integration_line_r, 'r--')
+        contour_points = np.array([s for s, w in contour])
+        plt.plot(contour_points.imag, contour_points.real)
         plt.show()
 
+    return {'name': 'srr_pair_combined_poles',
+            'results' : {'s': s_refined.simple_view()},
+            'rtol': {'s': 1e-5}}
 
-def test_srr_pair_separate_poles(plot_figures=False):
+
+def test_srr_pair_combined_poles():
+    helpers.run_test(srr_pair_combined_poles, tests_filename)
+test_srr_pair_combined_poles.__doc__ = srr_pair_combined_poles.__doc__
+
+
+def srr_pair_separate_poles(plot=False):
     "Cauchy integral for poles of an SRR pair considered as a single part"
 
     sim = openmodes.Simulation(basis_class=LoopStarBasis,
                                operator_class=EfieOperator)
 
-    parameters = {'inner_radius': 2.5e-3,
-                  'outer_radius': 4e-3}
-    mesh = sim.load_mesh(osp.join(openmodes.geometry_dir, 'SRR.geo'),
-                         parameters=parameters, mesh_tol=0.5e-3)
+    mesh = sim.load_mesh(meshfile)
     srr1 = sim.place_part(mesh)
     srr2 = sim.place_part(mesh, location=[0, 0, 1e-3])
 
-    s_min = -0.5e11-1e9j
-    s_max = -2e6+1.2e11j
-    integration_line_i = np.array((s_min.imag, s_max.imag, s_max.imag,
-                                   s_min.imag, s_min.imag))
-    integration_line_r = np.array((s_min.real, s_min.real, s_max.real,
-                                   s_max.real, s_min.real))
+    # calculate modes of each SRR
+    contour = ExternalModeContour(-0.5e11+1.2e11j, overlap_axes=0.2e9)
+    estimate = sim.estimate_poles(contour, parts=[srr1, srr2])
+    refined = sim.refine_poles(estimate)
 
-    # calculate modes of the SRR pair
-    result = sim.estimate_poles(RectangularContour(s_min, s_max),
-                                parts=[srr1, srr2])
-    refined = sim.refine_poles(result)
-
-    s_estimate = result[srr1].s
+    s_estimate = estimate[srr1].s
     s_refined = refined[srr1].s
 
-    s_reference = [-1.07078080e+09 + 4.44309178e+10j,
-                   -2.46067038e+10 + 9.46785130e+10j]
-    assert_allclose(s_refined[0], s_reference, rtol=1e-5)
-
-    if plot_figures:
+    if plot:
         plt.figure(figsize=(6, 4))
         plt.plot(s_estimate.imag, s_estimate.real, 'x')
+        plt.plot()
         plt.plot(s_refined.imag, s_refined.real, '+')
-        plt.plot(integration_line_i, integration_line_r, 'r--')
+        contour_points = np.array([s for s, w in contour])
+        plt.plot(contour_points.imag, contour_points.real)
         plt.show()
+        
+    return {'name': 'srr_pair_separate_poles',
+            'results' : {'s_srr1': s_refined[0].simple_view()},
+            'rtol': {'s_srr1': 1e-5}}
+
+def test_srr_pair_separate_poles():
+    helpers.run_test(srr_pair_separate_poles, tests_filename)
+test_srr_pair_separate_poles.__doc__ = srr_pair_separate_poles.__doc__
+
 
 if __name__ == "__main__":
-    test_srr_pair_combined_poles(plot_figures=True)
-    test_srr_pair_separate_poles(plot_figures=True)
+    # Uncomment the following lines to update reference solutions
+    # generate_mesh()
+    # helpers.create_reference(srr_pair_combined_poles, tests_filename)
+    # helpers.create_reference(srr_pair_separate_poles, tests_filename)
+
+    srr_pair_combined_poles(plot=True)
+    srr_pair_separate_poles(plot=True)
